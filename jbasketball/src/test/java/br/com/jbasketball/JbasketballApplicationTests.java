018f93616f4ccd71095d0341e9fdf14ca381191c
Joabe Pereira <joabe.infoti@yahoo.com.br>
2020-09-29 20:26:29 -0300

    Commit 1 - Início do projeto
    Commit 1 - Início do projeto

---
+++ jbasketball/src/test/java/br/com/jbasketball/JbasketballApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jbasketball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbasketballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
